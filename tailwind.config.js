64c3137c28f486f8cbf378466b3b6fbeabe9d53c
mumina <muminaabdi13@gmail.com>
2023-04-01 07:27:17 -0400

    navbar created, intro created, skills created, projects started. figure out why padding wont work on project div

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'blush-pink': "#E392B6",
+      }
+    },
+    fontFamily: {
+      'text-font': ['Roboto Condensed']
+    },
+
+    screens: {
+      desktop: '1280px',
+      laptop: '1024px',
+      mobile: '375px'
+    }
   },
   plugins: [],
 }
