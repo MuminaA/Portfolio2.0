fdffd67d0d3226367fb3fe77e689e01a001cb5b3
mumina <muminaabdi13@gmail.com>
2023-04-02 06:40:07 -0400

    projects portion created with responsiveness, footer created, nav fixed, and tailwindCSS error fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
-        'blush-pink': "#E392B6",
-      }
+        "blush-pink": "#E392B6",
+      },
+      padding: {
+        "1rem": "1rem",
+      },
     },
     fontFamily: {
-      'text-font': ['Roboto Condensed']
+      "text-font": ["Roboto Condensed"],
     },
-
     screens: {
-      desktop: '1280px',
-      laptop: '1024px',
-      mobile: '375px'
-    }
+      desktop: "1280px",
+      laptop: "1024px",
+      mobile: "375px",
+    },
   },
   plugins: [],
-}
-
+};
